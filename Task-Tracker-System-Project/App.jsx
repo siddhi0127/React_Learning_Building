873b4ed57d34e1ec755300828e96623111d381a1
Siddhi Potdar <146317589+siddhi0127@users.noreply.github.com>
2026-08-25 11:16:50 +0530

    Refactor task input and display logic

--- Task-Tracker-System-Project/App.jsx
+++ Task-Tracker-System-Project/App.jsx
@@ -11,12 +11,41 @@ function App() {
     <h2>Manage Your Tasks Efficiently and Stay Organized</h2>
     <input type="text" 
            placeholder="Enter new task" 
-           value={task} onChange={(e) => setTask(e.target.value)} />
+           value={task} onChange={(e) => setTask(e.target.value)} 
+           style={{height: '3rem',width: '18rem'}}/>
 
-     <button onClick={(e)=>{setTasks([...tasks, task]); setTask("")}}>Add new Task +</button>
-
+     <button onClick={(e)=>{setTasks([...tasks, 
+     {
+      id:Date.now(),
+      title : task,
+      completed : false
+     }]); 
+      setTask("")}} 
+     style={{marginLeft: '3rem', height: '3rem'}}>
+      Add new Task +</button>
+    
      {tasks.map((task)=>(
-      <p key={task}>{task}</p>
+      <div key={task.id}>
+        <input type="checkbox" 
+               checked={task.completed} 
+               onChange={() => {
+                 setTasks(
+            tasks.map((t) =>
+                t.id === task.id
+                    ? { ...t, completed: !t.completed }
+                    : t
+            )
+        );
+    }}/>
+        <span>  {task.title}</span>
+        <span style={{ marginLeft: '6rem' }}>
+          {task.completed ? "Completed" : "Pending"}
+        </span>
+
+       <button onClick={()=> setTasks(tasks.filter((t) => t.id !== task.id))} 
+       style={{marginLeft: '8rem'}}>Delete</button>
+
+      </div>
     ) )}
 
     </>
